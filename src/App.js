0e650e95119ad47c70cb571e1232261ff6d54ba4
Jeet solanki <solankijeet285@gmail.com>
2024-11-17 11:15:54 +0530

    Removed unnecesory imports so that netlify detect update and. the site will live again with updates

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import Header from "./components/Header";
-import Home from "./components/Home";
+
 import Projects from "./components/Projects";
 import Contact from "./components/Contact";
 import Footer from "./components/Footer";
